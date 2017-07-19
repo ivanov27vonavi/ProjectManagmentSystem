efc09a63a4941409dd7dd62ba882447d7901724e
ivanov27vonavi <cphgroup@ukr.net>
2017-07-19 11:21:26 +0300

    Homework 2.0

---
+++ src/projectmanager/ProjectManager.java
@@ -0,0 +1,14 @@
+
+package projectmanager;
+
+import javax.swing.JFrame;
+import org.nlt.view.MainFrame;
+
+public class ProjectManager {
+
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+        
+    }
+    
+}
